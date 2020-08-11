fe10960ae80d8872406670695fd49b411241f8fb
pirlo7764 <zfy@users.noreply.github.com>
2020-08-11 17:34:14 +0800

    Update 27.py

--- binary_tree/zfy/27.py
+++ binary_tree/zfy/27.py
@@ -2,12 +2,15 @@ class Solution:
     def reverse(self,treenew,treeold):
         if treeold is None:
             return
-        treenew.left=TreeNode(treeold.right.val)
-        treenew.right=TreeNode(treeold.left.val)
-        reverse(treenew.left,treeold.right)
-        reverse(treenew.right,treeold.left)
+        if treeold.right is not None:
+            treenew.left=TreeNode(treeold.right.val)
+        if treeold.left is not None:
+            treenew.right=TreeNode(treeold.left.val)
+        self.reverse(treenew.left,treeold.right)
+        self.reverse(treenew.right,treeold.left)
     def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return root
         rootnew=TreeNode(root.val)
         self.reverse(rootnew,root)
         return rootnew
-        
